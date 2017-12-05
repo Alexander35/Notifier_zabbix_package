3da0907054442e9492fe0cc3faf5ca13907f7ac0
Alexander Ivanov <alexander.ivanov.35@gmail.com>
2017-12-05 13:22:35 +0900

    named requests in requests.json

--- zabbix_notifier.py
+++ zabbix_notifier.py
@@ -19,17 +19,21 @@ class ZabbixNotifier(GeneralClient):
 			self.logger.error('Unable to init a Notifier {}'.format(exc))	
 			quit(1)	
 
-	def search_reqest(self, req, num):
-		self.logger.info('Perform a request {}'.format( self.Request_list[req][num]))
-		return self.Request_list[req][num]	
+	def search_reqest(self, req, req_name):
+		result = [js_res for js_res in self.Request_list[req] if 'req_name' in js_res if js_res['req_name'] == req_name]
+		if result == []:
+			self.logger.error('Can not find the {}'.format(req_name))
+			quit(0)
+		del result[0]['req_name']
+		return result[0]
 
-	def request(self, req, num):
+	def request(self, req, req_name):
 		result = self.zapi.do_request(
 				req, 
-				self.search_reqest(req, num)
+				self.search_reqest(req, req_name)
 			)
 		self.logger.info('request result \n{}\n'.format(result))
-		MSG = self.prepare_msg(result, '{} [{}]'.format(req, num))
+		MSG = self.prepare_msg(result, '{} [{}]'.format(req, req_name))
 		self.send(MSG)
 
 	def prepare_msg(self, raw_data, request):
@@ -47,10 +51,10 @@ def main():
 	parser.add_argument("-c", "--config", dest="conf", help="configuration file name", default="config.ini")
 	parser.add_argument("-l", "--log", dest="log", help="log file name", default="ZabbixNotifier")
 	parser.add_argument("task", default=None)
-	parser.add_argument("number", type=int, default=None)
+	parser.add_argument("req_name", default=None)
 	args = parser.parse_args()
 	ZN = ZabbixNotifier(ConfigName=args.conf, LoggerName=args.log)
-	ZN.request(args.task, args.number)
+	ZN.request(args.task, args.req_name)
 
 if __name__ == '__main__':
 	main()
